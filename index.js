cc11e8dce8ec634fd9d58dca163fd08c338c1059
NoJStein <127149391+NoJStein@users.noreply.github.com>
2024-02-12 13:17:19 -0800

    Third Commit
    Added subtraction, multiplication, and division
    Added Decimals

--- index.js
+++ index.js
@@ -4,6 +4,75 @@ function init() {
     const screen = document.getElementById('screen');
     console.log(screen);
 
+    function limitCheck() {
+        //console.log(screen.innerHTML.toString().length);    //Test
+
+        let limit = 20;
+
+        if (screen.innerHTML.toString().length > limit) {
+            console.log("overflow!")
+            screen.innerHTML = screen.innerHTML.substring(0, limit);
+        }
+    }
+
+    function isDecimal(num) {
+        for (i of num) {
+            if (i === '.') {return true}
+        }
+        return false;
+    }
+
+    function rounding(num) {
+        let zeroCount = 0;
+        let wasNine = false;
+
+        console.log("29 The number to round is ", num);        //Test
+
+        if (isDecimal(num)) {
+            console.log("32 Decimal Here")     //Test
+
+            for (let i = num.length - 1; i > -1; i--) {
+                console.log("35 current num = ", num[i]);      //Test
+                //console.log("36 zeroCount = ", zeroCount);     //Test
+
+                /*
+                                if (num[i] !== '0' && zeroCount > 1) {
+                    console.log("39 trimming")
+                    zeroCount = 0;
+                    num = num.substring(0, (i + 1));
+                    console.log(num);       //Test
+                } else if (num[i] === '0') {
+                    console.log("44 num[i] = ", num[i]);        //Test
+                    console.log("45 zeroCount = ", zeroCount);     //Test
+                    zeroCount++;
+                }
+                */
+
+                 /* else if (wasNine) {
+                    console.log("55 wasNine = ", wasNine);      //Test
+                    console.log("56 num[i] = ", num[i]);        //Test
+                    //let temp = parseInt(num[i]);
+                    //temp++;
+                    let charCode = num.charCodeAt(i);
+                    charCode += 1;
+                    let incrementedChar = String.fromCharCode(charCode);
+                    console.log(incrementedChar);       //Test
+
+
+                    console.log("58 num[i] = ", num[i]);        //Test
+                    wasNine = false;
+                }
+                */
+
+            }
+        }
+
+        console.log("final number is ", num);       //Test
+
+        return num;
+    }
+    
+
     const numberButtons = document.querySelectorAll('.button--num');
     numberButtons.forEach(button => {
         button.addEventListener('click', () => {
@@ -13,6 +82,8 @@ function init() {
             } else {
                 screen.innerHTML += button.dataset.value;
             }
+
+            limitCheck();
         })
     })
 
@@ -25,7 +96,7 @@ function init() {
             screen.dataset.operation = button.dataset.operation;
             screen.innerHTML = "0";
             //console.log(button.dataset.operation);
-            console.log(screen.dataset);
+            //console.log(screen.dataset);    //Test
         })
     })
     
@@ -33,20 +104,59 @@ function init() {
     equalsButton.addEventListener('click', () => {
         if (screen.dataset.state === "first") { return; }
         
-        console.log("Ans");
+        console.log("Ans");     //Test
 
         let answer = 0;
 
-        //console.log(screen.dataset.operation);
+        if (isDecimal(screen.dataset.hold) || isDecimal(screen.innerHTML)) {
+            switch (screen.dataset.operation) {
+                case "plus":
+                    answer = `${parseFloat(screen.dataset.hold) + parseFloat(screen.innerHTML)}`;
+                    break;
+    
+                case "minus":
+                    answer = `${parseFloat(screen.dataset.hold) - parseFloat(screen.innerHTML)}`;
+                    break;
+                
+                case "multiply":
+                    answer = `${parseFloat(screen.dataset.hold) * parseFloat(screen.innerHTML)}`;
+                    break;
+            }
+        } else {
+            switch (screen.dataset.operation) {
+                case "plus":
+                    answer = `${parseInt(screen.dataset.hold) + parseInt(screen.innerHTML)}`;
+                    break;
+    
+                case "minus":
+                    answer = `${parseInt(screen.dataset.hold) - parseInt(screen.innerHTML)}`;
+                    break;
+                
+                case "multiply":
+                    answer = `${parseInt(screen.dataset.hold) * parseInt(screen.innerHTML)}`;
+                    break;
+            }
+        }
 
         switch (screen.dataset.operation) {
-            case "plus":
-                answer = `${parseInt(screen.dataset.hold) + parseInt(screen.innerHTML)}`;
-                break;
+            case "divide":
+                if (screen.innerHTML === '0') {
+                    //answer = 'ERROR';
+                    console.log("Attempted division by zero!");
+                    break;
+                } else {
+                    answer = `${parseFloat(screen.dataset.hold) / parseFloat(screen.innerHTML)}`;
+                }
+                 
         }
+
         
+        
+
         // Update the screen with the new answer
-        screen.innerHTML = answer;
+        //rounding(answer);
+        screen.innerHTML = rounding(answer);
+        limitCheck();
         // Update the hold with the new answer
         screen.dataset.hold = answer;
 
@@ -63,6 +173,22 @@ function init() {
         console.log(screen.dataset);
     })
 
+/*
+    const decimalButton = document.querySelector('.button--decimal');
+    decimalButton.addEventListener('click', () => {
+        console.log("STOP CLICKING ME!");   //Test
+        for (i of screen.innerHTML) {
+            console.log(i);
+            if (i === '.') {return}
+        }
+        screen.innerHTML += decimalButton.dataset.value;
+        
+    })
+*/
+
+
 }
+
+
 
 document.addEventListener("DOMContentLoaded", init);
